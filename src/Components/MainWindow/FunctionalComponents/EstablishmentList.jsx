2ab3d36356d674d42d6f7245b518127e6e1cc71f
Daniil Ivanov <ivanovda010101@gmail.com>
2024-01-31 17:22:58 +0300

    MASTER: added MainPlace

--- src/Components/MainWindow/FunctionalComponents/EstablishmentList.jsx
+++ src/Components/MainWindow/FunctionalComponents/EstablishmentList.jsx
@@ -1,12 +1,23 @@
+import {StyledComponents, StyledEstablishmentList} from "../Styles/StyledComponents";
+
 export const EstablishmentList = ({ establishments }) => {
+    const estMock ={
+        id: 1,
+        name: "name",
+        imageUrl: "https://learnrussianlanguage.net/wp-content/uploads/2019/11/64a49ba01cbd346245cf2f019689f59d.jpg"
+    }
+    for (let i = 0; i < 9; i++) { // выведет 0, затем 1, затем 2
+        establishments = establishments.concat(estMock)
+    }
+    // establishments
     return (
-        <div>
+        <StyledEstablishmentList>
             {establishments.map((establishment) => (
-                <div key={establishment.id}>
+                <StyledComponents key={establishment.id}>
                     <img src={establishment.imageUrl} alt={establishment.name} />
                     <p>{establishment.name}</p>
-                </div>
+                </StyledComponents>
             ))}
-        </div>
+        </StyledEstablishmentList>
     );
 }
--- src/Components/MainWindow/FunctionalComponents/Switchers.jsx
+++ src/Components/MainWindow/FunctionalComponents/Switchers.jsx
@@ -1,10 +1,12 @@
+import {StyledButton} from "../Styles/StyledComponents";
+
 export const Switchers = ({ options, selectedOption, onOptionSelect }) => {
     return (
         <div>
             {options.map((option) => (
-                <button key={option} onClick={() => onOptionSelect(option)} className={option === selectedOption ? "selected" : ""}>
+                <StyledButton key={option} onClick={() => onOptionSelect(option)} className={option === selectedOption ? "selected" : ""}>
                     {option}
-                </button>
+                </StyledButton>
             ))}
         </div>
     );
