dec9e3c92bed27cb470357eb7285c25069893e14
Mekan777-alt <mekan2678@gmail.com>
2024-02-01 15:17:03 +0300

    add footer

---
+++ src/Components/Footer/Styles/StyledComponents.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+
+export const StyledFooter = styled.nav`
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    padding: 20px;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+`
+
+export const StyledLink = styled.div`
+    margin-right: 20px;
+`
---
+++ src/Components/Footer/Viewers/Footer.jsx
@@ -0,0 +1,15 @@
+import StyledFooter from "../Styles/StyledComponents";
+
+export const Footer = () => {
+
+    const textMain = "Главная"
+    const textAboutMe = "О нас"
+
+
+    return (
+        <StyledFooter>
+
+        </StyledFooter>
+    )
+
+}
