d3b1535ba7e6a6370b4507158bc37d3066d10e46
Mekan777-alt <mekan2678@gmail.com>
2024-02-02 16:30:18 +0300

    footer

--- src/Components/Footer/Viewers/Footer.jsx
+++ src/Components/Footer/Viewers/Footer.jsx
@@ -1,6 +1,12 @@
-import StyledFooter from "../Styles/StyledComponents";
+import {
+    StyledFooter,
+    StyledFooterBottom,
+    StyledFooterBottomA,
+    StyledFooterBottomH3,
+    StyledFooterBottomP, StyledFooterSpan, Title,
+} from "../Styles/StyledComponents";
 
-export const Footer = () => {
+const Footer = () => {
 
     const textMain = "Главная"
     const textAboutMe = "О нас"
@@ -8,8 +14,36 @@ export const Footer = () => {
 
     return (
         <StyledFooter>
-
+            <div style={{display:"flex", flexDirection:"column", alignItems:"center", textAlign:'center'}}>
+            <div>
+                <h2 style={{color:"orange"}}>Table<span style={{color:"yellow"}}>Book</span></h2>
+            </div>
+            <div>
+                <ul style={{display: "flex", gap:"20px", listStyle:"none", cursor:"pointer"}}>
+                    <a >Services</a>
+                    <a>About us</a>
+                    <a>Contact us</a>
+                    <a>Faqs</a>
+                    <a>Sign in</a>
+                </ul>
+            </div>
+                <div style={{display: "flex", justifyContent: "space-between", alignItems:"center", gap: "20px"}}>
+                    <div>
+                        <img src="./iamge.jpg"/>
+                    </div>
+                    <div>
+                        <img/>
+                    </div>
+                    <div>
+                        <img/>
+                    </div>
+                    <div>
+                        <h3>TableBooky.com | All rights reserve</h3>
+                    </div>
+                </div>
+            </div>
         </StyledFooter>
-    )
+    );
 
 }
+export default Footer
