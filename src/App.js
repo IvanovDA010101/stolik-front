1309f0193d13d3e681cdebc08518e818864fa7af
Daniil Ivanov <ivanovda010101@gmail.com>
2024-02-05 02:26:02 +0300

    RESTAURANT-CARD: added booking

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import {Main} from "./Components/MainWindow/Main";
 import {Route, Routes} from "react-router";
 import {RestaurantPageMain} from "./Components/RestaurantPage/RestaurantPageMain";
 import Footer from "./Components/Footer/Viewers/Footer";
-import {Header} from "./Components/MainWindow/Viewers/Header";
-import {MyFabricComponent} from "./Components/RestaurantScheme";
-import {BookingService} from "./Components/RestaurantPage/Viewers/BookingService";
+import {Admin} from "./Components/Admin/Admin";
 
 
 function App() {
@@ -15,8 +13,8 @@ function App() {
         <Routes>
             <Route path="/" element={<Main/>}/>
             <Route path="/restaurant/:id" element={<RestaurantPageMain/>}/>
-            <Route path="/restaurant/:id/tables/:tableId" element={<BookingService/>}/>
-            <Route path="/scheme/" element={<MyFabricComponent/>}/>
+            <Route path="/restaurant" element={<RestaurantPageMain/>}/>
+            <Route path="/admin" element={<Admin/>}/>
         </Routes>
         <Footer />
     </div>
