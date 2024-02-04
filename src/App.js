8494264bbb71453abdfcb3438b5461624beff065
Daniil Ivanov <ivanovda010101@gmail.com>
2024-02-04 14:48:34 +0300

    RESTAURANT-CARD: admin's scheme restaurant

--- src/App.js
+++ src/App.js
@@ -2,18 +2,15 @@ import './App.css';
 import {Main} from "./Components/MainWindow/Main";
 import {Route, Routes} from "react-router";
 import {RestaurantPageMain} from "./Components/RestaurantPage/RestaurantPageMain";
-import {Header} from "./Components/MainWindow/Viewers/Header";
-import {MyFabricComponent} from "./Components/RestaurantScheme";
+
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Main/>}/>
-        <Route path="/restaurant/:id" element={<RestaurantPageMain/>}/>
-        <Route path="/scheme/" element={<MyFabricComponent/>}/>
-      </Routes>
+        <Routes>
+            <Route path="/" element={<Main/>}/>
+            <Route path="/restaurant" element={<RestaurantPageMain/>}/>
+        </Routes>
     </div>
   );
 }
