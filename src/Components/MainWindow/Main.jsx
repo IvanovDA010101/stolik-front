846fa9028358770e329cebdb54c45c00156500bd
Daniil Ivanov <ivanovda010101@gmail.com>
2024-02-05 11:07:07 +0300

    RESTAURANT-CARD: added booking

--- src/Components/MainWindow/Main.jsx
+++ src/Components/MainWindow/Main.jsx
@@ -2,14 +2,12 @@ import {MainPlace} from "./Viewers/MainPlace";
 import {Filter} from "./Viewers/Filter";
 import {EstablishmentsMap} from "./Viewers/EstablishmentsMap";
 import {Footer} from "./Viewers/Footer";
-import {Header} from "./Viewers/Header";
 
 
 export const Main = () => {
 
     return (
         <div>
-            <Header/>
             <MainPlace/>
             <Filter/>
             <EstablishmentsMap/>
